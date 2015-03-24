145afcc94a31f984bc9e60f20079030085d54420
Alexis Lopez <alu0100204148@ull.edu.es>
2015-03-24 21:29:55 +0000

    Pruebas mocha + chai

---
+++ test/tests.js
@@ -0,0 +1,38 @@
+var assert = chai.assert;
+
+suite('Comma Separated Values', function() {
+
+  setup(function(){
+    if (typeof __html__ !== 'undefined') {
+      document.body.innerHTML = __html__['test/index.html'];
+      input = document.getElementById('INPUT');
+      ouput = document.getElementById('OUTPUT');
+    }
+  });
+  
+  
+  
+  
+   test('Prueba declaracion de variable.', function() {
+    INPUT.value = 'var a = "hello";';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "hello",\n        "arity": "literal"\n    }\n}');
+  });
+   
+   
+  
+  test('Prueba declaracion de una funcion.', function() {
+  	INPUT.value = 'var a = "hello"; // initialize a var b = function(x) {var c = 3;return x+c; };';
+  	main();
+  	assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "hello",\n        "arity": "literal"\n    }\n}');
+  });
+  
+  
+  test('Prueba de error', function() {
+    INPUT.value = 'var = var a';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "name": "TypeError",\n    "message": "undefined is not a function"\n}');
+  });
+  
+
+});
