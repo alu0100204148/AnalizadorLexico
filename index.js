eb1318b9ce3915ebc76f4202b5b9accfff751e48
Alexis Lopez <alu0100204148@ull.edu.es>
2015-03-24 20:32:26 +0000

    Configurando heroku

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (request, response) {
+    response.render('index', { title: 'Comma Separated Value Analyzer' });
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
