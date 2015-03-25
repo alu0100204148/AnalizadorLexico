684a2648a1a9315881d51b552c10c1f2caec241a
Marcos Luis <alu0100777086@ull.edu.es>
2015-03-25 20:03:16 +0000

    Pruebas para tokens.js y parse.js

--- test/tests.js
+++ test/tests.js
@@ -22,13 +22,33 @@ suite('Analizador lexico', function() {
    
   
   test('Prueba declaracion de una funcion.', function() {
-  	INPUT.value = 'var a = "hello"; // initialize a var b = function(x) {var c = 3;return x+c; };';
+  	INPUT.value = 'var a = "hello"; // initialize a \nvar b = function(x) {var c = 3;return x+c; };';
   	main();
-  	assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "hello",\n        "arity": "literal"\n    }\n}');
+  	assert.deepEqual(OUTPUT.innerHTML, '[\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": "hello",\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "b",\n            "arity": "name"\n        },\n        "second": {\n            "value": "function",\n            "arity": "function",\n            "first": [\n                {\n                    "value": "x",\n                    "arity": "name"\n                }\n            ],\n            "second": [\n                {\n                    "value": "=",\n                    "arity": "binary",\n                    "first": {\n                        "value": "c",\n                        "arity": "name"\n                    },\n                    "second": {\n                        "value": 3,\n                        "arity": "literal"\n                    }\n                },\n                {\n                    "value": "return",\n                    "arity": "statement",\n                    "first": {\n                        "value": "+",\n                        "arity": "binary",\n                        "first": {\n                            "value": "x",\n                            "arity": "name"\n                        },\n                        "second": {\n                            "value": "c",\n                            "arity": "name"\n                        }\n                    }\n                }\n            ]\n        }\n    }\n]');
   });
   
+  test('Prueba statement', function() {
+  	INPUT.value = 'var a = 2;\nif(a ===2){}';
+  	main();
+  	assert.deepEqual(OUTPUT.innerHTML, '[\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": 2,\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "if",\n        "arity": "statement",\n        "first": {\n            "value": "===",\n            "arity": "binary",\n            "first": {\n                "value": "a",\n                "arity": "name"\n            },\n            "second": {\n                "value": 2,\n                "arity": "literal"\n            }\n        },\n        "second": null,\n        "third": null\n    }\n]');
+  });
   
- 
+  test('Prueba syntax error', function() {
+    INPUT.value = 'var a = $';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '"Syntax error near \'$\'"');
+  });
   
+  test('Prueba require();', function() {
+    INPUT.value = 'require("hola");';
+    main();
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "require",\n    "arity": "statement",\n    "first": {\n        "value": "hola",\n        "arity": "literal"\n    }\n}');
+  });
+  
+  test('Prueba var=require();', function() {
+    INPUT.value = 'var a = require("hola");';
+    main();'{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "require",\n        "arity": "function",\n        "first": {\n            "value": "hola",\n            "arity": "literal"\n        }\n    }\n}'
+    assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "require",\n        "arity": "function",\n        "first": {\n            "value": "hola",\n            "arity": "literal"\n        }\n    }\n}');
+  });
 
 });
